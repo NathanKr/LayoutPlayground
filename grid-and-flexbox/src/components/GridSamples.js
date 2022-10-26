305cce95a6c3cd7f2cf575857ab825e3aca86b66
Nathan Krasney <natankrasney@gmail.com>
2022-10-26 11:37:16 +0300

    ...

--- grid-and-flexbox/src/components/GridSamples.js
+++ grid-and-flexbox/src/components/GridSamples.js
@@ -5,7 +5,6 @@ import GridSampleNesting from './GridSampleNesting'
 import GridSample3 from "./GridSample3";
 import "../styles/GridSamples.css";
 import GridSampleComplex1 from "./GridSampleComplex1";
-import GridSampleComplex2 from "./GridSampleComplex2";
 
 
 
@@ -13,7 +12,6 @@ const GridSamples = () => {
   return (
     <div>
       <GridSampleComplex1/>
-      <GridSampleComplex2/>
       <GridSample1 />
       <GridSample2 />
       <GridSampleNesting />
